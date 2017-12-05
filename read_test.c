bff717cfc724691d93d192fea5b007b0775a93b3
Ganna DANYLOVA <gdanylov@e1r11p2.unit.ua>
2017-12-05 21:43:13 +0200

    allcheck

--- read_test.c
+++ read_test.c
@@ -18,119 +18,91 @@
 #include <stdio.h>
 #include "libft.h"
 
-// int	ft_counts(char *s)
-// {
-// 	int i;
-// 	int qsymb;
-// 	int counttetrims;
-
-// 	i = 0;
-// 	qsymb = 0;
-// 	while (s[i] != '\0')
-// 	{
-// 		if (s[i] == '.' || s[i] == '#' || s[i] == '\n')
-// 			i++;
-// 		qsymb = i;
-// 		i = 0;
-// 		if (s[i] != '\n')
-// 		{
-// 			while (s[i] != '\n' && s[i] != '\0')
-// 				i++;
-// 			counttetrims++;
-// 		}
-// 		else
-// 			return (0);
-// 		return(qsymb);
-// 	}
-// 	return (counttetrims);
-// }
-
-int	counttetrims(char *s)
+int		check_one(char *s, int *i)
 {
-	int i;
-	int y;
-	int count;
+	int		count;
+	int		y;
+	int		r;
 
-	i = 0;
-	while (s[i])
+	count = 0;
+	y = 0;
+	r = 0;
+	while (s[*i] && y < 4)
 	{
-		y = 0;
-		count = 0;
-		while (s[i] && y < 4)
+		if (s[*i] == '\n' && count == 4)
 		{
-			if (s[i] == '\n' && count == 4)
-			{
-				y++;
-				count = 0;
-				i++;
-			}
-			if (s[i] == '\0')
-				break ;
-			if ((s[i] != '#' || s[i] != '.') || (s[i] != '\n' && count == 4))
-				return (0);
-			i++;
-			count++;
+			y++;
+			count = 0;
+			*i += 1;
 		}
-		if ((s[i] && s[i] != '\n') || (y != 4))
+		else if (s[*i] && ((s[*i] != '#' && s[*i] != '.')
+		|| (s[*i] != '\n' && count == 4)))
 			return (0);
-		i++;
+		(s[*i] == '#') ? r++ : r;
+		(s[*i] != '\0' && y != 4) ? *i += 1 : *i;
+		count++;
 	}
+	if ((s[*i] && s[*i] != '\n') || (y != 4) || (r != 4))
+		return (0);
 	return (1);
 }
-// 	{
-// 		if (s[i] == '.' || s[i] == '#')
-// 			i++;
-// 		printf("%d\n", i);
-// 		if (s[i] != '.' || s[i] != '#')
-// 		{
-// 			while (s[i] != '\n' && s[i] != '\0')
-// 				i++;
-// 			qtetrim++;
-// 		}
-// 	}
-// 	one = i;
-// 	printf("%d\n", one);
-// 	printf("%d\n", qtetrim);
-// 	return (qtetrim);
-// }
-// 	// if (s[i] != '\n')
-// 	// {
-// 	// 	while (s[i] != '\n' )
-// 	// 		i++;
-// 	// }
-// 	// // if (s[i] == '\0')
-// 	// // 	return (0);
-// 	// if (qsymb == 20)
-// 	// 	return (1);
-// 	// else
-// 	// 	return (0);
 
-int main(int argc, char **argv)
+int		ft_check(char *s)
 {
-	int fd;
-	char *buf;
-	int tetrims;
-	int count;
+	int		i;
+	int		check;
 
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-		return (1);
+	i = 0;
+	check = 0;
+	while (s[i])
+	{
+		if (check_one(s, &i) == 0)
+			return (0);
+		if (s[i] == '\0')
+		{
+			check = 1;
+			break ;
+		}
+		i++;
+	}
+	if (check == 0)
+		return (0);
+	return (1);
+}
+
+int		read_file(int fd)
+{
+	char	*buf;
+	int		tetrims;
+
+	tetrims = 0;
 	buf = ft_strnew(545);
 	while (read(fd, buf, 544))
-	{
 		tetrims++;
-	}
 	if (tetrims != 1)
 	{
 		ft_putstr("error\n");
 		return (0);
 	}
 	ft_putstr(buf);
-	// printf("%s\n", buf);
-	if (counttetrims(buf) == 1)
-		ft_putstr("ok");
+	if (ft_check(buf) == 1)
+		ft_putstr("ok\n");
 	else
-		ft_putstr("ko");
+		ft_putstr("ko\n");
+	if ((ft_check) == 0)
+		return (0);
+	return (1);
+}
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+
+	(void)argc;
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		return (1);
+	read_file(fd);
 	if (close(fd) == -1)
 		return (1);
 	return (0);
